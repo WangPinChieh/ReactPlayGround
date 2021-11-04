c0038562544d6ccb127e49eb3efbb4147d7b1c4d
Jay Wang <jay.wang@hp.com>
2021-11-04 10:08:03 +0800

    refactor multi-select

--- src/multi-select.js
+++ src/multi-select.js
@@ -3,29 +3,13 @@ import Select from "react-select";
 import makeAnimated from "react-select/animated";
 import lodash from "lodash";
 function MultiSelect({ options, value, onChange, ...props }) {
-  /*
-  Input value: ['jenniferId', 'pvId']
 
-  Conver to 
-  [{label: 'jennifer.lin2@hp.com', value: 'jenniferId'},
-  {label: 'ray.huang@hp.com', value: 'pvId'}]
-
-  Output value: 
-  [{label: 'jennifer.lin2@hp.com', value: 'jenniferId'},
-  {label: 'ray.huang@hp.com', value: 'pvId'}]
-
-  Conver to 
-  ['jenniferId', 'pvId']
-  */
-
-  const optionsMap = lodash.groupBy(options, (option) => {
+  const optionsMap = lodash.groupBy(lodash.cloneDeep(options), (option) => {
     return option.value;
   });
-  const defaultSelectedOptions = lodash.cloneDeep(value ?? []);
-  const mappedDefaultSelectedOptions = defaultSelectedOptions.map((option) => {
+  const mappedDefaultSelectedOptions = lodash.cloneDeep(value ?? []).map((option) => {
     return optionsMap[option][0];
   });
-  const animatedComponents = makeAnimated();
 
   const _handleChange = (selectedOptions) => {
     let mappedOptions = selectedOptions.map((option) => {
@@ -33,12 +17,11 @@ function MultiSelect({ options, value, onChange, ...props }) {
     });
     onChange(mappedOptions);
     value = mappedOptions;
-    console.log(mappedOptions);
   };
   return (
     <Select
       closeMenuOnSelect={false}
-      components={animatedComponents}
+      components={makeAnimated()}
       isMulti
       options={options}
       onChange={_handleChange}
