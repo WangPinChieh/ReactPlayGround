26c9e23d84925032b5298458f62bea9d9daec4f8
Jay Wang <jay.wang@hp.com>
2021-11-04 17:48:09 +0800

    update working multi select in starliner

--- src/multi-select.js
+++ src/multi-select.js
@@ -1,47 +1,53 @@
-import React from "react";
-import Select from "react-select";
-import makeAnimated from "react-select/animated";
-import lodash from "lodash";
-import PropTypes from "prop-types";
+import React from 'react';
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
+import lodash from 'lodash';
+import PropTypes from 'prop-types';
+
 const propTypes = {
-  options: PropTypes.arrayOf(
-    PropTypes.shape({
-      value: PropTypes.string,
-      label: PropTypes.string,
-    })
-  ),
-  value: PropTypes.arrayOf(PropTypes.string),
-  onChange: PropTypes.func,
+	options: PropTypes.arrayOf(
+		PropTypes.shape({
+			value: PropTypes.string,
+			label: PropTypes.string,
+		}),
+	),
+	value: PropTypes.arrayOf(PropTypes.string),
+	placeholder: PropTypes.string,
+	onChange: PropTypes.func,
 };
 const defaultProps = {
-  onChange: () => {},
-  options: [],
-  value: [],
+	onChange: () => {},
+	options: [],
+	value: [],
+	placeholder: '',
 };
-function MultiSelect({ options, value, onChange }) {
-  const optionsMap = lodash.groupBy(lodash.cloneDeep(options), (option) => {
-    return option.value;
-  });
-  let mappedSelectedOptions = lodash.cloneDeep(value ?? []).map((option) => {
-    return optionsMap[option]?.[0];
-  });
 
-  const _handleChange = (selectedOptions) => {
-    let mappedOptions = selectedOptions.map((option) => {
-      return option.value;
-    });
-    onChange(mappedOptions);
-  };
-  return (
-    <Select
-      closeMenuOnSelect={false}
-      components={makeAnimated()}
-      isMulti
-      options={options}
-      onChange={_handleChange}
-      value={mappedSelectedOptions}
-    />
-  );
+function MultiSelect({ options, placeholder, value, onChange }) {
+	const optionsMap = lodash.groupBy(lodash.cloneDeep(options), option => {
+		return option.value;
+	});
+	let mappedSelectedOptions = lodash.cloneDeep(value ?? []).map(option => {
+		return optionsMap[option]?.[0];
+	});
+	const _handleChange = selectedOptions => {
+		let mappedOptions = selectedOptions.map(option => {
+			return option.value;
+		});
+
+		onChange(mappedOptions);
+	};
+
+	return (
+		<Select
+			closeMenuOnSelect={false}
+			components={makeAnimated()}
+			isMulti
+			options={options}
+			onChange={_handleChange}
+			value={mappedSelectedOptions}
+			placeholder={placeholder}
+		/>
+	);
 }
 
 MultiSelect.propTypes = propTypes;
